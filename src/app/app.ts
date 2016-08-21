a1402ef07e54fd1f35e746d49e437c8c39e0d98c
Mehul Patel <mehul8@gmail.com>
2016-08-20 20:30:35 -0700

    init commit

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <div>
+      <h3>
+        Yo
+      </h3>
+    </div>
+  `
+})
+
+export class App {}
